d0511a6a0dcb44f6d1d11c4860f1014226c07ca0
Cuihaokang <531846872@qq.com>
2019-04-22 22:48:02 +0800

    finish static pages

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-//RESTful资源控制器和路由
-// Route::resource('articles', 'ArticlesController');
-
-//分开来写路由
-Route::get('/articles', 'ArticlesController@index')->name('articles.index');
-// Route::get('/articles/{id}', 'ArticlesController@show')->name('articles.show');
-Route::get('create', 'ArticlesController@create')->name('articles.create');
-Route::post('/articles', 'ArticlesController@store')->name('articles.store');
-Route::get('/articles/{id}/edit', 'ArticlesController@edit')->name('articles.edit');
-Route::patch('/articles/{id}', 'ArticlesController@update')->name('articles.update');
-Route::delete('/articles/{id}', 'ArticlesController@destroy')->name('articles.destroy');
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
