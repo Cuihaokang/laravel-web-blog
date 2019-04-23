481ea6f431db6b527dfc9919c68d1cfebcd5e39a
Cuihaokang <531846872@qq.com>
2019-04-23 15:52:11 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,12 @@
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="row">
+        <div class="col-sm-6 col-md-3 index-footer">.col-xs-4 .col-md-3</div>
+        <div class="col-sm-6 col-md-3 index-footer">.col-xs-4 .col-md-3</div>
+        <div class="col-sm-6 col-md-3 index-footer">.col-xs-4 .col-md-3</div>
+        <div class="col-sm-6 col-md-3 index-footer">.col-xs-4 .col-md-3</div>
+      </div>
+    </div>
+  </div>
+</div>
