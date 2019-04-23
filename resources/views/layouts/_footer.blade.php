a956d59f953c685876b51cbbea5e2b7cd738aa7b
Cuihaokang <531846872@qq.com>
2019-04-23 16:08:24 +0800

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -5,7 +5,7 @@
         <div class="col-sm-6 col-md-3 index-footer">.col-xs-4 .col-md-3</div>
         <div class="col-sm-6 col-md-3 index-footer">.col-xs-4 .col-md-3</div>
         <div class="col-sm-6 col-md-3 index-footer">.col-xs-4 .col-md-3</div>
-        <div class="col-sm-6 col-md-3 index-footer">.col-xs-4 .col-md-3</div>
+        <div class="col-sm-6 col-md-3 index-footer"><a href="{{ route('about') }}">关于</a></div>
       </div>
     </div>
   </div>
